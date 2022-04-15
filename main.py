53bcc89a43eb676341a3480379731c28c068600e
ksnsjsbshjs <103725319+ksnsjsbshjs@users.noreply.github.com>
2022-04-15 11:33:36 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from pyrogram import Client
-from pyrogram import filters
 
-bot = Client(
-   session_name="tanovercos",
-    api_id=14027438,
-    api_hash="9c9527c9c3e59853818ba0b0e2486737",
-    bot_token="5179626757:AAFgODzhEXbx-oHqWU_x2huZzWNYeS6iz5U"
-)
-
-@bot.on_message(filters.private & filters.command(['start']))
-async def start(client, message):
-    await message.reply_text(text="Hello welcome!", reply_to_message_id=message.message_id, parse_mode="markdown")
-
-bot.run()
