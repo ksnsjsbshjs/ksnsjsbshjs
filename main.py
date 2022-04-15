1d669856b0527c1640c48ae0b138843f263156d6
ksnsjsbshjs <103725319+ksnsjsbshjs@users.noreply.github.com>
2022-04-15 10:21:41 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,91 @@
-from pyrogram import Client
-from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent,
-                            InlineKeyboardMarkup, InlineKeyboardButton)
-from pyrogram import filters
-import logging
-logging.basicConfig(filename='mylog.log', level=logging.ERROR)
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import timezone
+from random import choice
+from datetime import datetime
+from re import match
+from pyrogram.raw.functions.account import UpdateProfile
+from pyrogram import Client, idle
+from pyrogram.types import Message
+from pyrogram.filters import command, edited
+timeN, timeB = False, False
+main = '9876543210'
+dots = ['•',
+       '⁃', 
+       '⦾',
+       '⁍',
+       '‣',
+       '¤'
+]
+fonts = [
+    '❾➇➆➅➄➃➂❷❶⓪',
+    '➒➑➐➏➎➍➌➋➊⓿',
+    '𝟫𝟪𝟩𝟨𝟧𝟦𝟥𝟤𝟣𝟢',
+    '𝟿𝟾𝟽𝟼𝟻𝟺𝟹𝟸𝟷𝟶',
+    '𝟡𝟠𝟟𝟞𝟝𝟜𝟛𝟚𝟙𝟘',
+    '𝟵𝟴𝟳𝟲𝟱𝟰𝟯𝟮𝟭𝟬',
+    '𝟗𝟖𝟕𝟔𝟓𝟒𝟑𝟐𝟏𝟎',
+    '９８７６５４３２１０',
+    'ၦᲖᒣᦆƼᔰᗱᒿ᧒ᦲ',
+    '68ㄥގ9ㄣƐᄅ⇂0',
+    '₉₈₇₆₅₄₃₂₁₀',
+    '⁹⁸⁷⁶⁵⁴³²¹⁰',
+    '⑨⑧⑦⑥⑤④③②①⓪',
+    '९𝟠7ϬƼ५Ӡϩ𝟙⊘'
+]
+def creatTime():
+    time = datetime.now(timezone('Asia/Tehran')).strftime(f'%H{choice(dots)}%M')
+    time = time.translate(time.maketrans(main, choice(fonts)))
+    return time
+app = Client('myAcc', 14027438, '9c9527c9c3e59853818ba0b0e2486737', phone_number='989303689793') #PhoneNumber
+time = int()
+def changeTime():
+    global time
+    minute = datetime.now().minute
+    if time != minute:
+        timeFont = creatTime() 
+        try:
+            app.send(UpdateProfile(last_name=timeFont if timeN else None, about=timeFont if timeB else None))
+        except ConnectionError:
+            pass
+    time = minute
+with app:
+    mention = app.get_me().mention
+help = f"""**Hi {mention} 🖐🏻
+  • TimeName: [./]timename <on|off>
+  • TimeBio: [./]timebio <on|off>**"""
+isOn = '• {} is {}'
+@app.on_message(edited)
+def editedMessage(_, m: Message):
+    pass
+@app.on_message(command(['help'], prefixes=['.', '/']))
+def helpMenu(_, m: Message):
+    m.edit(help)
+@app.on_message(command('timename', prefixes=['.', '/']))
+def timeName(app, m: Message):
+    global timeN
+    command = m.command
+    try:
+        if match(r"(?i)on", command[1]):
+            timeN = True
+        else:
+            timeN = False
+        m.edit(isOn.format('Time in Name', 'on' if timeN else 'off'))
+    except IndexError:
+        m.edit(help)
+@app.on_message(command('timebio', prefixes=['.', '/']))
+def timeBio(app, m: Message):
+    global timeB
+    command = m.command
+    try:
+        if match(r"(?i)on", command[1]):
+            timeB = True
+        else:
+            timeB = False
+        m.edit(isOn.format('Time in Bio', 'on' if timeN else 'off'))
+    except IndexError:
+        m.edit(help)
 
-bot = Client(
-   session_name="tanovercos",
-    api_id=14027438,
-    api_hash="9c9527c9c3e59853818ba0b0e2486737",
-    bot_token="5181588402:AAEx5GzBQNZ3PY-h5usaRFa-55fKzaSxNwY"
-)
-
-@bot.on_inline_query()
-def answer(client, inline_query):
-    inline_query.answer(
-        results=[
-            InlineQueryResultArticle(
-                title="creator",
-                input_message_content=InputTextMessageContent(
-                    "salam baraye payam dadan be man ruye in click kon"
-                ),
-                url="https://t.me/tan_over_cos",
-                description="id man",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton(
-                            "click me",
-                            url="https://t.me/tan_over_cos"
-                        )]
-                    ]
-                )
-            ),
-          
-        ],
-        cache_time=1
-    )
-  
-@bot.on_message(filters.private & filters.command(['start']))
-async def start(client, message):
-    await message.reply_text(text="your start", reply_to_message_id=message.message_id, parse_mode="markdown")
-
-bot.run()  # Automatically start() and idle()
+scduler = AsyncIOScheduler()
+scduler.add_job(changeTime, 'interval', seconds=1)
+scduler.start()
+app.start(), print('started...'), idle(), app.stop()
